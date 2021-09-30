47687e6da5a1598612d42b39155f1f77c156babe
August Saint Freytag <night@apricummedia.com>
2021-09-30 11:17:54 +0200

    Define general purpose argument error model

---
+++ Color Theme Utility/Modules/Common/Errors/ArgumentError.swift
@@ -0,0 +1,14 @@
+//
+//  Color Theme Utility
+//
+//  Created by August Saint Freytag on 30/09/2021.
+//
+
+import Foundation
+
+/// An error related to command line arguments and their format.
+struct ArgumentError: Error, LocalizedError {
+	
+	var errorDescription: String?
+	
+}
