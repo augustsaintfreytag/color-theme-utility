b58f67948830885acc0f7b450cc730d0237058be
August Saint Freytag <night@apricummedia.com>
2021-10-02 12:00:27 +0200

    Define error structs for theme coding and conversion

---
+++ Color Theme Utility/Modules/Common/Errors/ThemeCodingError.swift
@@ -0,0 +1,14 @@
+//
+//  Color Theme Utility
+//
+//  Created by August Saint Freytag on 02/10/2021.
+//
+
+import Foundation
+
+/// An error related to encoding and decoding theme models.
+struct ThemeCodingError: Error, LocalizedError {
+	
+	var errorDescription: String?
+	
+}
---
+++ Color Theme Utility/Modules/Common/Errors/ThemeConversionError.swift
@@ -0,0 +1,14 @@
+//
+//  Color Theme Utility
+//
+//  Created by August Saint Freytag on 02/10/2021.
+//
+
+import Foundation
+
+/// An error that occurred while processing and mapping themes.
+struct ThemeConversionError: Error, LocalizedError {
+	
+	var errorDescription: String?
+	
+}
