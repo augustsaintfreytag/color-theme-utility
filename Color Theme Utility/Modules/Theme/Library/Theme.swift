f0cff88df358f15d105c1a73a35eca238df453ac
August Saint Freytag <night@apricummedia.com>
2021-10-02 12:07:55 +0200

    Establish base theme type and Xcode theme model, define coding logic
    Define mapping via coding keys for Xcode theme.

---
+++ Color Theme Utility/Modules/Theme/Library/Theme.swift
@@ -0,0 +1,29 @@
+//
+//  Color Theme Utility
+//
+//  Created by August Saint Freytag on 02/10/2021.
+//
+
+import Foundation
+
+/// A theme of a fixed collection of color values, used for a specific domain or for exchange.
+protocol Theme {}
+
+extension Theme where Self: Codable {
+	
+	/// A pretty-printed description of the encoded theme contents for debugging.
+	var formattedEncodedDebugDescription: String? {
+		let debugPrintEncoder = JSONEncoder()
+		debugPrintEncoder.outputFormatting = [.sortedKeys, .prettyPrinted]
+		
+		guard
+			let data = try? debugPrintEncoder.encode(self),
+			let string = String(data: data, encoding: .utf8)
+		else {
+			return nil
+		}
+		
+		return string
+	}
+	
+}
