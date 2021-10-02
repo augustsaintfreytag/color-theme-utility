5a23e5b5525815239c41e953447072bd2e1ec488
August Saint Freytag <night@apricummedia.com>
2021-10-02 10:50:00 +0200

    Establish properties for master theme model

--- Color Theme Utility/Modules/Theme/Models/Theme.swift
+++ Color Theme Utility/Modules/Theme/Models/Theme.swift
@@ -6,4 +6,45 @@
 
 import Foundation
 
-struct Theme {}
+struct Theme: Codable {
+	
+	let foreground: Color
+	let background: Color
+	let selectionBackground: Color
+	let activeLineBackground: Color
+	let insertionPoint: Color
+	
+	let comment: Color
+	let commentDocumentation: Color
+	let commentSection: Color
+	let commentSectionHeader: Color
+	
+	let keyword: Color
+	let functionProject: Color
+	let functionSystem: Color
+	let functionParameter: Color
+	let preprocessorProject: Color
+	let preprocessorSystem: Color
+	let constantProject: Color
+	let constantSystem: Color
+	let variableProject: Color
+	let variableSystem: Color
+	let typeProject: Color
+	let typeSystem: Color
+	let referenceTypeProject: Color
+	let referenceTypeSystem: Color
+	let valueTypeProject: Color
+	let valueTypeSystem: Color
+	let enumProject: Color
+	let enumSystem: Color
+	let declarationType: Color
+	let declarationAny: Color
+	let attribute: Color
+	let module: Color
+	
+	let number: Color
+	let string: Color
+	let character: Color
+	let url: Color
+	
+}
