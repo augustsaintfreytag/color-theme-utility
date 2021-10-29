bf34f202337bd3f72698a4c69f79417619531ac6
August Saint Freytag <night@apricummedia.com>
2021-10-29 12:26:32 +0200

    Make color format detector and modeler static, displace form with format

--- ColorThemeFramework/Modules/Color/Library/ColorModeler.swift
+++ ColorThemeFramework/Modules/Color/Library/ColorModeler.swift
@@ -12,11 +12,16 @@ public protocol ColorModeler: ColorFormatDetector {}
 extension ColorModeler {
 	
 	/// Detects the format of the given color string and creates a `Color` model for supported types.
-	public func color(fromAutodetectedColorString string: String) -> Color? {
+	public static func color(fromAutodetectedColorString string: String) -> Color? {
 		guard let format = colorFormat(for: string) else {
 			return nil
 		}
 		
+		return color(from: string, format: format)
+	}
+	
+	/// Creates and returns a `Color` from the given string, assuming the provided color format.
+	public static func color(from string: String, format: ColorFormat) -> Color? {
 		switch format {
 		case .floatRGBA:
 			return Color(fromFloatRGBAString: string)
