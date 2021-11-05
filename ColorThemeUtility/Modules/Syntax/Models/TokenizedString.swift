7611c756c0bf6f1b56f66bc711c7b90950d52520
August Saint Freytag <night@apricummedia.com>
2021-11-05 11:07:49 +0100

    Define new tokenized preset for protocol definition

--- ColorThemeUtility/Modules/Syntax/Models/TokenizedString.swift
+++ ColorThemeUtility/Modules/Syntax/Models/TokenizedString.swift
@@ -106,15 +106,25 @@ extension TokenizedString {
 
 extension TokenizedString {
 	
-	public enum TokenizedPresets {
+	public enum Presets {
 		
 		public static var structDefinition: TokenizedString {
-			return TokenizedString(tokens: [
+			TokenizedString(tokens: [
 				.word("struct", as: \.keyword), .space,.word("ObjectMetadata", as: \.declarationType), .word(":"), .space, .word("Hashable", as: \.referenceTypeSystem), .word(","), .space, .word("ObjectProperty", as: \.referenceTypeProject), .space, .word("{"), .space, .newLine,
 				.indent, .word("let", as: \.keyword), .space, .word("id", as: \.declarationAny), .word(":"), .space, .word("UUID", as: \.referenceTypeSystem), .newLine,
 				.indent, .word("let", as: \.keyword), .space, .word("created", as: \.declarationAny), .word(":"), .space, .word("Date", as: \.referenceTypeSystem), .newLine,
 				.indent, .word("let", as: \.keyword), .space, .word("owners", as: \.declarationAny), .word(":"), .space, .word("Set", as: \.referenceTypeSystem), .word("<"), .word("Owner", as: \.valueTypeProject), .word("."), .word("Identifier", as: \.valueTypeProject), .word(">"), .newLine,
 				.indent, .word("let", as: \.keyword), .space, .word("data", as: \.declarationAny), .word(":"), .space, .word("String", as: \.typeSystem), .newLine,
+				.word("}")
+			])
+		}
+		
+		public static var protocolWithFunctionDefinition: TokenizedString {
+			TokenizedString(tokens: [
+				.word("protocol", as: \.keyword), .space, .word("ObjectProvider", as: \.declarationType), .space, .word("{"), .newLine,
+				.indent, .word("func", as: \.keyword), .space, .word("groupedObjects", as: \.declarationAny),
+				.word("("), .word("_", as: \.declarationAny), .space, .word("collection", as: \.functionParameter), .word(":"), .space, .word("["), .word("Object", as: \.valueTypeProject), .word("]"), .word(")"),
+				.space, .word("->"), .space, .word("["), .word("ObjectGroup", as: \.valueTypeProject), .word(":"), .space, .word("Set", as: \.valueTypeSystem), .word("<"), .word("Object", as: \.valueTypeProject), .word(">"), .word("]"), .newLine,
 				.word("}")
 			])
 		}
