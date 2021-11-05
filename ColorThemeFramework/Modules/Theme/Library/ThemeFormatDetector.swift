a1df86e670e3b92e458cb124a3245e5d5b8a8c49
August Saint Freytag <night@apricummedia.com>
2021-11-05 11:06:34 +0100

    Introduce format detection for themes in protocol, define new import
    Allow theme import with automatic detection of encoded format.

---
+++ ColorThemeFramework/Modules/Theme/Library/ThemeFormatDetector.swift
@@ -0,0 +1,32 @@
+//
+//  Color Theme Utility
+//
+//  Created by August Saint Freytag on 05/11/2021.
+//
+
+import Foundation
+
+/// Functionality to detect the format of themes from their encoded forms (raw data)
+/// via heuristics known about supported types.
+public protocol ThemeFormatDetector {}
+
+extension ThemeFormatDetector {
+	
+	/// Auto-detects the representative format of the given encoded theme data.
+	public func themeFormat(for encodedThemeData: Data) -> ThemeFormat {
+		guard let dataString = String(data: encodedThemeData, encoding: .utf8) else {
+			return .unknown
+		}
+		
+		if dataString.contains(IntermediateTheme.defaultHeader) {
+			return .intermediate
+		}
+		
+		if dataString.contains("DVTFontAndColorVersion") {
+			return .xcode
+		}
+		
+		return .unknown
+	}
+	
+}
--- ColorThemeFramework/Modules/Theme/Library/ThemeImporter.swift
+++ ColorThemeFramework/Modules/Theme/Library/ThemeImporter.swift
@@ -7,10 +7,33 @@
 import Foundation
 
 /// Functionality to process file paths, read and decode theme file contents into their appropriate model.
-public protocol ThemeImporter {}
+public protocol ThemeImporter: ThemeFormatDetector {}
 
 extension ThemeImporter {
 	
+	// MARK: Theme In
+	
+	/// Auto-detects the format the encoded theme uses and decodes it to its corresponding type.
+	public func decodedTheme(from data: Data) throws -> Theme {
+		let format = themeFormat(for: data)
+		
+		switch format {
+		case .intermediate:
+			let decoder = JSONDecoder()
+			return try decoder.decode(IntermediateTheme.self, from: data)
+		case .xcode:
+			let decoder = PropertyListDecoder()
+			return try decoder.decode(XcodeTheme.self, from: data)
+		default:
+			throw ThemeCodingError(description: "Decoding theme data with format '\(format.rawValue)' is not supported.")
+		}
+	}
+	
+	// MARK: Data In
+	
+	/// TODO: Rework import functions to `throw` for errors, move assertion to command.
+	
+	/// Reads the data of an encoded theme from the file at the given path.
 	public func encodedDataFromFileContents(from path: String) -> Data? {
 		guard
 			let themeUrl = URL(fileURLWithNonCanonicalPath: path),
@@ -22,6 +45,7 @@ extension ThemeImporter {
 		return data
 	}
 	
+	/// Reads the data of an encoded theme from the file at the given path.
 	public func encodedDataFromFileContents(from url: URL) -> Data? {
 		do {
 			return try Data(contentsOf: url)
