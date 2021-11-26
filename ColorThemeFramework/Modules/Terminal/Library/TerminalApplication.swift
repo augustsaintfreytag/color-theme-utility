2273b80ef20560cd4738660b62a5082dd065241c
August Saint Freytag <night@apricummedia.com>
2021-11-26 13:19:03 +0100

    Introduce types for terminal application detection

---
+++ ColorThemeFramework/Modules/Terminal/Library/TerminalApplication.swift
@@ -0,0 +1,13 @@
+//
+//  Color Theme Utility
+//
+//  Created by August Saint Freytag on 26/11/2021.
+//
+
+import Foundation
+
+/// A type denoting a supported terminal application.
+public enum TerminalApplication {
+	case apple
+	case iterm
+}
