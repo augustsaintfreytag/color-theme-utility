1417a2a8d98d7db744fdaaea7b820059e506e399
August Saint Freytag <night@apricummedia.com>
2022-01-18 11:25:59 +0100

    Introduce color theme assembly target in project
    Create dedicated target intended for WebAssembly compilation target.

---
+++ ColorThemeAssembly/ColorThemeAssembly.h
@@ -0,0 +1,17 @@
+//
+//  ColorThemeUtility
+//
+//  Created by August Saint Freytag on 14/01/2022.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ColorThemeAssembly.
+FOUNDATION_EXPORT double ColorThemeAssemblyVersionNumber;
+
+//! Project version string for ColorThemeAssembly.
+FOUNDATION_EXPORT const unsigned char ColorThemeAssemblyVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ColorThemeAssembly/PublicHeader.h>
+
+
