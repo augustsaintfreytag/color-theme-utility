2da2c6df7213c04d9d7ef03ecd2ba750a764a7e1
August Saint Freytag <night@apricummedia.com>
2022-01-19 13:10:56 +0100

    Remove unneeded dependency JavaScript Kit after testing

--- Package.swift
+++ Package.swift
@@ -11,18 +11,13 @@ let package = Package(
 	products: [
 		.executable(name: "ColorThemeAssembly", targets: ["ColorThemeAssembly"])
 	],
-	dependencies: [
-		.package(name: "JavaScriptKit", url: "https://github.com/swiftwasm/JavaScriptKit", from: "0.11.1")
-	],
+	dependencies: [],
 	targets: [
 		// Targets are the basic building blocks of a package. A target can define a module or a test suite.
 		// Targets can depend on other targets in this package, and on products in packages this package depends on.
 		.executableTarget(
 			name: "ColorThemeAssembly",
-			dependencies: [
-				.product(name: "JavaScriptKit", package: "JavaScriptKit"),
-				"ColorThemeModelingFramework"
-			],
+			dependencies: ["ColorThemeModelingFramework"],
 			path: "ColorThemeAssembly"
 		),
 		.target(
