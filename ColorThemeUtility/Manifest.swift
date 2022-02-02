aedade3fa8a4d5458805910dca18f3bb5bf0def5
August Saint Freytag <night@apricummedia.com>
2022-02-02 11:46:36 +0100

    Define utility manifest, update command config. to use manifest data
    Print detailed name, version, and build in "--help" command.

---
+++ ColorThemeUtility/Manifest.swift
@@ -0,0 +1,30 @@
+//
+//  ColorThemeUtility
+//
+//  Created by August Saint Freytag on 02/02/2022.
+//
+
+import Foundation
+
+/// The application's manifest.
+enum Manifest {
+	
+	// MARK: Data
+	
+	static let name = "Color Theme Utility"
+	static let version = "0.3.2"
+	
+	// MARK: Formatting
+	
+	private static var releaseVersionDescription: String { version }
+	private static var debugVersionDescription: String { "\(version) Debug Preview" }
+	
+	static var versionDescription: String {
+		#if DEBUG
+		return debugVersionDescription
+		#else
+		return releaseVersionDescription
+		#endif
+	}
+	
+}
