10bce0c2289933e1f36d4b67f747abc80ebd9281
August Saint Freytag <night@apricummedia.com>
2022-02-18 11:11:29 +0100

    Introduce theme enclosure writer protocol
    Update project references, update target configuration for enclosure
    framework and related properties.

---
+++ ColorThemeEnclosureFramework/Modules/ThemeEnclosure/Library/ThemeEnclosureWriter.swift
@@ -0,0 +1,40 @@
+//
+//  ColorThemeUtility
+//
+//  Created by August Saint Freytag on 17/02/2022.
+//
+
+import Foundation
+import ColorThemeModelingFramework
+import ColorThemeCodingFramework
+
+public protocol ThemeEnclosureWriter: VisualStudioCodeThemeEnclosureWriter {}
+
+extension ThemeEnclosureWriter {
+	
+	public static func writeTheme(_ theme: Theme, to path: URL) throws {
+		switch theme {
+		case let intermediateTheme as IntermediateTheme:
+			try writeUnenclosedTheme(intermediateTheme, to: path)
+		case let xcodeTheme as XcodeTheme:
+			try writeUnenclosedTheme(xcodeTheme, to: path)
+		case let textMateTheme as TextMateTheme:
+			try writeUnenclosedTheme(textMateTheme, to: path)
+		case let visualStudioCodeTheme as VisualStudioCodeTheme:
+			try writeEnclosedTheme(visualStudioCodeTheme, to: path)
+		default:
+			throw ThemeCodingError(description: "Generated theme data with format '\(theme.typeFormat)' can not be output.")
+		}
+	}
+	
+	private static func writeUnenclosedTheme<CodableTheme: Theme & Encodable>(_ theme: CodableTheme, to outputDirectoryPath: URL) throws {
+		let encoding = defaultEncoding(for: theme)
+		let encodedTheme = try Self.encodedTheme(theme, as: encoding)
+		
+		let fileName = "Theme.\(encoding.rawValue)"
+		let filePath = outputDirectoryPath.appendingPathComponent(fileName)
+		
+		try encodedTheme.write(to: filePath, atomically: false, encoding: .utf8)
+	}
+	
+}
